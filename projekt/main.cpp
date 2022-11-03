e46b7952e67766dc6a9b27d08599f98c3eba0a9e
student <student@it11-20.it.local>
2022-11-03 13:33:47 +0100

    commit

---
+++ projekt/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    cout << "Program sluzy do liczenia miejsca zerowego dla rownania liniowego." << endl;
+    cout << "Funkcja ma postac: f(x)=a*x+b" << endl << endl;
+    float a, b;
+    cout << "Podaj a: ";
+    cin >> a;
+    cout << "Podaj b: ";
+    cin >> b;
+    //miejsce zerowe to: 0=a*x+b <=> -b=a*x  <=> (-b)/a=x
+    if( a != 0 )
+    {
+        cout << "Funkcja f(x) przyjmuje wartosc=0 dla x=" <<( - b ) / a << endl;
+    } else cout << "Funkcja f(x) nie posiada rozwiazania." << endl;
+
+
+    getch();
+    return( 0 );
+}
